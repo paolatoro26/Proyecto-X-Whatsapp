bde6978c628b29bda8c9c9323ac9a2c91f03b773
paola <paolatorobejar@gmail.com>
2017-01-25 18:12:45 -0500

    punto 1

--- js/main.js
+++ js/main.js
@@ -1,9 +1,31 @@
 
-var input=document.querySelector(".w-search-contacts");
+//Desaparecer placeholder en input de busqueda.
 
-input.addEventListener("click", eliminarPlaceholder);
+var inputBuscar=document.querySelector(".w-search-contacts");
+inputBuscar.addEventListener("focus", eliminarPlaceholder);
 
 function eliminarPlaceholder (evt)
 {	
-		evt.target.setAttribute("placeholder","");
+		evt.target.placeholder="";
 }
+
+//Ingresar el texto del input de mensajes.
+
+var inputMensaje = document.querySelector(".w-write-message-field");
+var divChatIn = document.querySelector(".w-message w-message-out");
+var fecha = new Date();
+var hora = fecha.getHours();
+
+inputMensaje.addEventListener("keyup", subirMensaje);
+
+function subirMensaje(evt)
+{
+	if(evt.keyCode==13)
+	{
+
+	}
+}
+
+
+
+
