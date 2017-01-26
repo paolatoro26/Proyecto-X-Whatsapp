a863ffaf27ceae4b23d912d18c2e0c762cb72a73
paola <paolatorobejar@gmail.com>
2017-01-26 17:04:57 -0500

    cambios js

--- js/main.js
+++ js/main.js
@@ -1,30 +1,82 @@
+//Ingresar mensaje
 
-//Desaparecer placeholder en input de busqueda.
-
-var inputBuscar=document.querySelector(".w-search-contacts");
-inputBuscar.addEventListener("focus", eliminarPlaceholder);
-
-function eliminarPlaceholder (evt)
-{	
-		evt.target.placeholder="";
+function persona (_avatar, _nombre)
+{
+	this.avatar =_avatar;
+	this.nombre=_nombre;
 }
 
+function mensaje(_mensajeIn,_mensajeOut)
+{
+	this.mensajeIn=_mensajeIn;
+	this.mensajeOut=_mensajeOut;
+}
+
+function whatsapp()
+{	
+	this.buscar= function()
+	{
+		var search = document.getElementById("search");
+		var contacto = document.getElementsByTagName("h4");
+		var forEach = Array.prototype.forEach;
+
+		search.addEventListener("keyup", function(e){
+
+		var choice = this.value;
+
+		forEach.call(contacto, function(f)
+			{
+	    		if (f.innerHTML.toLowerCase().search(choice.toLowerCase()) == -1)
+	        {
+	        	f.parentNode.parentNode.style.display = "none"; 	
+	        }
+	    		else
+	     	{
+	     		f.parentNode.parentNode.style.display = "block"; 
+	     	}
+	               
+		});
+	}, false);
+}
+	this.fecha=function
+	{
+		var actual=new Date();
+		var hora=actual.getHours();
+
+		return hora;
+	}
+}
+
+//Buscador de nombres
+
+
+
+//
+
+/*
 //Ingresar el texto del input de mensajes.
 
-var inputMensaje = document.querySelector(".w-write-message-field");
-var divChatIn = document.querySelector(".w-message w-message-out");
-var fecha = new Date();
-var hora = fecha.getHours();
+  	var fecha = new Date();
+	var	hora = fecha.getHours();
 
-inputMensaje.addEventListener("keyup", subirMensaje);
+inputMensaje.addEventListener("keydown", subirMensaje);
+
+function agregar()
+{
+	var input=document.querySelector(".w-write-message-field").value;
+	var divChatIn= document.querySelector(".w-message w-message-out");
+}
+	
+	
+}
+
 
 function subirMensaje(evt)
 {
-	if(evt.keyCode==13)
-	{
-
-	}
+	console.log(evt.keyCode);	
 }
+*/
+
 
 
 
