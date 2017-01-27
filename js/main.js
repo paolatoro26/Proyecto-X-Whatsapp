722b5448a63f8ada3a4d84034c17e92a082a095d
paola <paolatorobejar@gmail.com>
2017-01-27 16:13:58 -0500

    avance punto2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,110 @@
-//Ingresar mensaje
+function Chat()
+{
+	this.nombre = '';
+	this.people = [];
+	this.messages = [];
+	this.chatAvatar = '';
+}
 
+function Person(_name, _avatar)
+{
+	this.name = _name;
+	this.avatar = _avatar;
+}
+function Message(_message, _sender)
+{
+	this.message = _message;
+	this.sender = _sender;
+	this.received = false;
+}
+function Whatsapp()
+{
+	this.chats = [];
+	this.selectedChat = null;
+	this.searchChat		= function(_keyword){};
+	this.getChatFromId	= function(_chatId){};
+	this.drawChatList	= function(_htmlTarget){
+		
+	};
+	this.drawMessageList= function(){
+		var divChat = document.getElementById('chat');
+		divChat.innerHTML = '';
+
+		for (var i in this.selectedChat.messages) {
+			if (object.hasOwnProperty(i)) {
+				console.log(this.selectedChat.messages[i]);
+				this.sendMessage(this.selectedChat.messages[i], false);
+			}
+		}
+	};
+	this.getLastMessage = function(){
+		return this.selectedChat.messages[this.selectedChat.messages.length-1];
+	};
+	this.sendMessage	= function(_message, _in){
+		var htmlMessageIn = '<div class="w-message w-message-in"><div class="w-message-text"><p>' + _message.message + '</p><div class="time">14:27</div></div></div>';
+		var htmlMessageOut = '<div class="w-message w-message-out"><div class="w-message-text"><p>' + _message.message + '</p><div class="time">14:27</div></div></div>';
+		var divChat = document.getElementById('chat');
+
+		this.selectedChat.messages.push(_message);
+
+		if(_in)
+		{
+			divChat.innerHTML += htmlMessageIn;
+		}else{
+			divChat.innerHTML += htmlMessageOut;
+		}
+
+		divChat.scrollTop = divChat.scrollHeight;
+	};
+}
+
+
+var wapp = new Whatsapp();
+
+var me = new Person('Gerson');
+var zare = new Person('Zare');
+
+var chat = new Chat();
+
+chat.people.push(zare);
+
+wapp.chats.push(chat);
+wapp.selectedChat = chat;
+
+wapp.sendMessage(new Message('Hola', zare));
+wapp.sendMessage(new Message('Que tal?', me));
+
+console.log(wapp.getLastMessage().sender);
+
+
+window.onload = init;
+
+var inputMessage;
+var divChat;
+var chatPanel;
+
+function init()
+{
+	inputMessage = document.getElementById('mensajes');
+	divChat = document.getElementById('chat');
+	//chatPanel = document.querySelector('.w-chat-panel');
+
+	inputMessage.addEventListener('keyup', onInputKeyUp);
+}
+
+function onInputKeyUp(evt)
+{
+	console.log(evt.keyCode);
+	if(evt.keyCode == 13)
+	{
+		wapp.sendMessage(new Message(evt.target.value, me));
+		evt.target.value = '';
+	}
+}
+
+
+/*
+-----------------------------
 function persona (_avatar, _nombre)
 {
 	this.avatar =_avatar;
@@ -13,31 +118,10 @@ function mensaje(_mensajeIn,_mensajeOut)
 }
 
 function whatsapp()
-{	
 	this.buscar= function()
+{	
 	{
-		var search = document.getElementById("search");
-		var contacto = document.getElementsByTagName("h4");
-		var forEach = Array.prototype.forEach;
-
-		search.addEventListener("keyup", function(e){
-
-		var choice = this.value;
-
-		forEach.call(contacto, function(f)
-			{
-	    		if (f.innerHTML.toLowerCase().search(choice.toLowerCase()) == -1)
-	        {
-	        	f.parentNode.parentNode.style.display = "none"; 	
-	        }
-	    		else
-	     	{
-	     		f.parentNode.parentNode.style.display = "block"; 
-	     	}
-	               
-		});
-	}, false);
-}
+		
 	this.fecha=function
 	{
 		var actual=new Date();
@@ -46,36 +130,63 @@ function whatsapp()
 		return hora;
 	}
 }
-
+*/
 //Buscador de nombres
 
 
 
+var search = document.getElementById("search");
+var contacto = document.getElementsByTagName("h4");
+var forEach = Array.prototype.forEach;
+
+search.addEventListener("keyup", function(e){
+ var choice = this.value;
+
+ forEach.call(contacto, function(f){
+     if (f.innerHTML.toLowerCase().search(choice.toLowerCase()) == -1)
+         {
+         	f.parentNode.parentNode.style.display = "none";
+         }
+            
+     else
+     {
+     	f.parentNode.parentNode.style.display = "block"; 
+     }
+               
+ });
+}, 
+false);
 //
 
 /*
 //Ingresar el texto del input de mensajes.
 
-  	var fecha = new Date();
-	var	hora = fecha.getHours();
+var fecha = new Date();
+var	hora = fecha.getHours()+":"+fecha.getMinutes();
+var input=document.getElementById("mensajes");
+var chat=document.getElementById("chat");
+var mensajeOut = '<div class="w-message w-message-out"><div class="w-message-text"><p>' + input.value + '</p><div class="time">14:27</div></div></div>';
 
-inputMensaje.addEventListener("keydown", subirMensaje);
 
-function agregar()
-{
-	var input=document.querySelector(".w-write-message-field").value;
-	var divChatIn= document.querySelector(".w-message w-message-out");
-}
-	
-	
-}
-
+input.addEventListener("keydown", subirMensaje);
 
 function subirMensaje(evt)
 {
-	console.log(evt.keyCode);	
+	if( evt.keyCode == 13)
+	{
+		agregar();
+	}
 }
-*/
+
+function agregar()
+{
+	chat.push(mensajeOut);
+	input.value="";
+}
+	*/
+	
+
+
 
 
 
